7cbe56d7c1cdc136a75a08c76ccffb2516ed252c
haripurnomosidik <hari.rofalon.geous@gmail.com>
2021-04-24 17:42:18 +0800

    nestedloop ke 1

---
+++ nestedloop1.py
@@ -0,0 +1,14 @@
+
+for x in range (10, 0, -1):
+    for y in range (10, x-1,-1):
+        print(y, end=" ")
+    print()
+
+
+
+
+
+for x in range (10):
+    for y in range (10, x-1,-1):
+        print(y, end=" ")
+    print()
